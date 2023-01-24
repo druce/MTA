10a20c139e490bda832b61bb82169b4867a5b67a
Druce Vertes <drucev@gmail.com>
2023-01-23 20:32:41 -0500

    fix bad mapping of pelham pkwy and gun hill road

--- build-docker.sh
+++ build-docker.sh
@@ -3,6 +3,7 @@
 export BASEDIR="/mnt"
 export DBFILE="mta.db"
 export DATADIR="downloads"
+export FLASK_APP="superset"
 
 # export MAPBOX_API_KEY= see https://docs.mapbox.com/help/getting-started/access-tokens/
 # export ENABLE_TEMPLATE_PROCESSING=True or set in superset's config.py
--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 export BASEDIR="/Users/myuserid/projects/MTA"
 export DBFILE="mta.db"
 export DATADIR="downloads"
+export FLASK_APP="superset"
 
 # export MAPBOX_API_KEY= see https://docs.mapbox.com/help/getting-started/access-tokens/
 # export ENABLE_TEMPLATE_PROCESSING=True or set in superset's config.py
